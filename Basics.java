01120ac662e2ba639cbb5aa27299a8d8deaa22fa
Imaad Uddin <imaaduddin@icloud.com>
2021-03-12 16:41:59 -0600

    comments

--- Basics.java
+++ Basics.java
@@ -3,5 +3,15 @@ public class Basics { // this is a class or a collection of related code
     System.out.print("Hello World!"); // print statement in Java this prints everything on the same line.
     System.out.println("Hello World!!!"); // this will print on a new line as if you hit enter when typing a new text.
     System.out.println("This repo is about Java!"); // prints on a new line.
+    System.out.print("My name is Imaad!\n"); // the \n will have the text print on to a new line
+    System.out.print("\tMy name is Imaad!"); // the \t is tab and works like the tab key on your keyboard
+    System.out.print("\"My name is Imaad!\""); // To use double quotes within double quotes you have to pu a back slash before each double quote as shown.
+    System.out.print("My name is Imaad!\\"); // To display a backslash you have to type 2 backslashes 
+    // this is a comment, it is followed by forward slashes and is ignored by the compiler 
+    /* This is a multi line comment
+    It is also ignored 
+    By the compiler
+    But just lets you type things on multiple lines
+    */
   }
 }
