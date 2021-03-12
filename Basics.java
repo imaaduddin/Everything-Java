74e61fdd6aa9598a4fc84fab248d27e3aa7f8565
Imaad Uddin <imaaduddin@icloud.com>
2021-03-12 16:43:58 -0600

    cleaned file up

--- Basics.java
+++ Basics.java
@@ -1,13 +1,23 @@
-public class Basics { // this is a class or a collection of related code 
-  public static void main(String[] args) { // this the main method and our program will not run because we call the main method to compile and run code. Code in main method starts from the top and works down.
-    System.out.print("Hello World!"); // print statement in Java this prints everything on the same line.
-    System.out.println("Hello World!!!"); // this will print on a new line as if you hit enter when typing a new text.
-    System.out.println("This repo is about Java!"); // prints on a new line.
-    System.out.print("My name is Imaad!\n"); // the \n will have the text print on to a new line
-    System.out.print("\tMy name is Imaad!"); // the \t is tab and works like the tab key on your keyboard
+public class Basics { // This is a class or a collection of related code 
+  public static void main(String[] args) { // This the main method and our program will not run because we call the main method to compile and run code. Code in main method starts from the top and works down.
+    System.out.print("Hello World!"); // Print statement in Java this prints everything on the same line.
+
+    System.out.println("Hello World!!!"); // This will print on a new line as if you hit enter when typing a new text.
+
+    System.out.println("This repo is about Java!"); // Prints on a new line.
+
+    System.out.print("My name is Imaad!\n"); // The \n will have the text print on to a new line
+
+    System.out.print("\tMy name is Imaad!"); // The \t is tab and works like the tab key on your keyboard
+
     System.out.print("\"My name is Imaad!\""); // To use double quotes within double quotes you have to pu a back slash before each double quote as shown.
+
     System.out.print("My name is Imaad!\\"); // To display a backslash you have to type 2 backslashes 
+
+
     // this is a comment, it is followed by forward slashes and is ignored by the compiler 
+
+
     /* This is a multi line comment
     It is also ignored 
     By the compiler
