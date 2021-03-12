761bbd02a4143ed17abf20c17ef8646460c4ceb0
Imaad Uddin <imaaduddin@icloud.com>
2021-03-12 16:34:28 -0600

    basics and types of print statements

--- Basics.java
+++ Basics.java
@@ -1,3 +1,7 @@
-public class Basics {
-  
+public class Basics { // this is a class or a collection of related code 
+  public static void main(String[] args) { // this the main method and our program will not run because we call the main method to compile and run code. Code in main method starts from the top and works down.
+    System.out.print("Hello World!"); // print statement in Java this prints everything on the same line.
+    System.out.println("Hello World!!!"); // this will print on a new line as if you hit enter when typing a new text.
+    System.out.println("This repo is about Java!"); // prints on a new line.
+  }
 }
