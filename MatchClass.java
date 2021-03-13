5618a68db9cdd4c9b0236b6c56a10720b3e49e2f
Imaad Uddin <imaaduddin@icloud.com>
2021-03-12 18:38:17 -0600

    math class

---
+++ MatchClass.java
@@ -0,0 +1,36 @@
+// public class MatchClass {
+//   public static void main (String[] args) {
+//     double x = 3.14;
+//     double y = -10;
+
+    // can also do double z = Math.floor(x);
+    // System.out.println(z);
+
+//     System.out.println(Math.max(x, y));
+//     System.out.println(Math.min(x, y));
+//     System.out.println(Math.abs(y));
+//     System.out.println(Math.sqrt(x));
+//     System.out.println(Math.round(x));
+//     System.out.println(Math.ceil(x)); // round up
+//     System.out.println(Math.floor(x)); // round down
+//   }
+// }
+
+
+// Finding Hypotenuse 
+import java.util.Scanner;
+
+public class MatchClass {
+  public static void main(String[] args) {
+    double x;
+    double y;
+    double z;
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter side x: ");
+    x = scanner.nextDouble();
+    System.out.println("Enter side y: ");
+    y = scanner.nextDouble();
+  }
+}
