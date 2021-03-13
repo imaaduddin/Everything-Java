182407c0386d12387dfd1b0f027a5cb7a0a9a64d
Imaad Uddin <imaaduddin@icloud.com>
2021-03-12 18:08:37 -0600

    expressions

---
+++ Expressions.java
@@ -0,0 +1,17 @@
+public class Expressions {
+  public static void main(String[] args) {
+    // expressions = operands & operators
+    // operands = values, variables, numbers, quantity
+    // operators = + - * / %
+
+    int friends = 10;
+    friends++;
+    friends--;
+    int newFriend = friends * 2;
+    double newerFriend = (double) newFriend / 3;
+
+    System.out.println(friends);
+    System.out.println(newFriend);
+    System.out.println(newerFriend);
+  }
+}
