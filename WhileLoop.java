3d412c776dd50c083df6f8cc48fd20fe10263204
Imaad Uddin <imaaduddin@icloud.com>
2021-03-15 12:21:27 -0500

    while loop

---
+++ WhileLoop.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class WhileLoop {
+  public static void main(String[] args) {
+    // while loop executes a block of code as long as it's condition remains true 
+
+    Scanner scanner = new Scanner(System.in);
+    String name = "";
+
+    while (name.isBlank()) {
+      System.out.println("Enter your name: ");
+      name = scanner.nextLine();
+    }
+
+    // do while  
+    // do {
+    //   System.out.println("Enter your name: ");
+    //   name = scanner.nextLine();
+    // } while (name.isBlank());
+
+    System.out.println("Hello " + name);
+  }
+}
