736738a1a35a8833bf0f49da99c9d6e1fb3b5bcd
Imaad Uddin <imaaduddin@icloud.com>
2021-03-19 15:57:11 -0500

    objects

---
+++ Objects.java
@@ -0,0 +1,14 @@
+// OOP
+public class Objects {
+  public static void main(String[] args) {
+    // object = an instance of a class that may contain attributes and methods 
+    // example: (phone, desk, computer, coffee cup)
+    ObjectsTwo myCar = new ObjectsTwo();
+    ObjectsTwo myCar2 = new ObjectsTwo();
+
+    System.out.println(myCar.model);
+    myCar.brake();
+
+    
+  }
+}
---
+++ ObjectsTwo.java
@@ -0,0 +1,16 @@
+public class ObjectsTwo {
+  // below are attributes
+  String make = "Mercedes";
+  String model = "GT 63 AMG";
+  int year = 2021;
+  String color = "black";
+  double price = 120000.00;
+
+  void drive() {
+    System.out.println("You drive the car");
+  }
+
+  void brake() {
+    System.out.println("You step on the brakes");
+  }
+}
