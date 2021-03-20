20cd3d473fef67b85906ea42b9c5e7ca4d6d8d8e
Imaad Uddin <imaaduddin@icloud.com>
2021-03-20 15:14:55 -0500

    variable scope

---
+++ VariableScope.java
@@ -0,0 +1,9 @@
+public class VariableScope {
+  public static void main(String[] args) {
+    // local = declared inside a method, visible only to that method 
+
+    // global = declared outside a method, but within a class, visible to all parts of a class 
+
+    VariableScopeTwo diceRoller = new VariableScopeTwo();
+  }
+}
