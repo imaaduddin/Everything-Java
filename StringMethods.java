dd672954790d1081f48228fb8c31830d9d3d616e
Imaad Uddin <imaaduddin@icloud.com>
2021-03-23 18:06:05 -0500

    test

--- StringMethods.java
+++ StringMethods.java
@@ -10,10 +10,10 @@ public class StringMethods {
     // char result = name.charAt(0);
     // int result = name.indexOf("o");
     // boolean result = name.isEmpty();
-    // String result = name.toUpperCase();
+    String result = name.toUpperCase();
     // String result = name.toLowerCase();
     // String result = name.trim();
-    String result = name.replace("o", "a");
+    // String result = name.replace("o", "a");
 
     System.out.println(result);
   }
