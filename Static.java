ae1afe953ca97837b913fbe70c32fa7b4a4913eb
Imaad Uddin <imaaduddin@icloud.com>
2021-03-31 23:29:09 -0500

    static

---
+++ Static.java
@@ -0,0 +1,11 @@
+public class Static {
+  public static void main(String[] args) {
+    // static = modifier, a single copy of a variable/method is created and shared 
+    // the class "owns" the static member 
+
+    StaticTwo friendOne = new StaticTwo("Choji");
+    StaticTwo friendTwo = new StaticTwo("Shikimaru");
+
+    System.out.println(StaticTwo.numberOfFriends);
+  }
+}
---
+++ StaticTwo.java
@@ -0,0 +1,9 @@
+public class StaticTwo {
+  String name;
+  static int numberOfFriends;
+
+  StaticTwo(String name) {
+    this.name = name;
+    numberOfFriends++;
+  }
+}
